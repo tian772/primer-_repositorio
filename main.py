889be49e3871af5e1e4003b4f1324c5726bcff77
tian772 <160678393+tian772@users.noreply.github.com>
2024-08-23 21:24:32 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+meme_dict = {
+            "CRESTA": "la cima de un lugar o el punto mas alto",
+            "WEON": "es un termino chileno que se puede ocupar para referirce a una persona tonta o a un amigo",
+            }
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict.keys():
+    print( meme_dict[word])
+else:
+    print("su palabra no esta 😢!")
+   
